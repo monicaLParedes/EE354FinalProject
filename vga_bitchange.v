89b77c028d1b308d1d207527df2a0a25bc6b89f0
Monica Paredes <mlparede@usc.edu>
2024-11-20 19:22:23 -0800

    Changed demo to use skeleton for project
    up button makes the square move down

---
+++ vga_bitchange.v
@@ -0,0 +1,96 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:15:38 12/14/2017 
+// Design Name: 
+// Module Name:    vgaBitChange 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+// Date: 04/04/2020
+// Author: Yue (Julien) Niu
+// Description: Port from NEXYS3 to NEXYS4
+//////////////////////////////////////////////////////////////////////////////////
+module vga_bitchange(
+	input clk,
+	input bright,
+	input button,
+	input [9:0] hCount, vCount,
+	output reg [11:0] rgb,
+	output reg [15:0] score
+   );
+	
+	parameter BLACK = 12'b0000_0000_0000;
+	parameter WHITE = 12'b1111_1111_1111;
+	parameter RED   = 12'b1111_0000_0000;
+	parameter GREEN = 12'b0000_1111_0000;
+	parameter BLUE = 12'b0000_0000_1111;
+	integer count = 0;
+
+	wire whiteZone;
+	wire greenMiddleSquare;
+	reg reset;
+	reg[9:0] greenMiddleSquareY;
+	reg[49:0] greenMiddleSquareSpeed; 
+
+	initial begin
+		greenMiddleSquareY = 10'd200;
+		score = 15'd0;
+		reset = 1'b0;
+	end
+	
+	
+	always@ (*) // paint a white box on a red background
+    	if (~bright)
+		rgb = BLACK; // force black if not bright
+	 else if (greenMiddleSquare == 1)
+		rgb = BLUE;
+	 else if (whiteZone == 1)
+		rgb = WHITE; // white box
+	 else
+		rgb = BLACK; // background color
+
+	
+	always@ (posedge clk)
+		begin
+		greenMiddleSquareSpeed = 50'd0;
+		
+		if ((button == 1'b1) && (greenMiddleSquareY < 10'd475))
+		  begin
+		  count = count + 1;
+		  
+		  if ((count % 1000000) == 0)
+		      greenMiddleSquareY = greenMiddleSquareY + 10'd001;
+		  end
+		  
+		if (button == 1'b0)
+		  count = 0;
+	   end
+
+	/*always@ (posedge clk)
+		if ((reset == 1'b0) && (button == 1'b1) && (hCount >= 10'd144) && (hCount <= 10'd784) && (greenMiddleSquareY >= 10'd400) && (greenMiddleSquareY <= 10'd475))
+			begin
+			score = score + 16'd1;
+			reset = 1'b1;
+			end
+		else if (greenMiddleSquareY <= 10'd20)
+			begin
+			reset = 1'b0;
+			end*/
+
+	assign whiteZone = ((hCount >= 10'd144) && (hCount <= 10'd784)) && ((vCount >= 10'd100) && (vCount <= 10'd475)) ? 1 : 0;
+
+	assign greenMiddleSquare = ((hCount >= 10'd340) && (hCount < 10'd380)) &&
+				   ((vCount >= greenMiddleSquareY) && (vCount <= greenMiddleSquareY + 10'd40)) ? 1 : 0;
+	
+endmodule
